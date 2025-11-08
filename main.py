03e9fae2995ba503f096b8a5303426424a7a62e7
RashidRJ <rashidrj.johnson@gmail.com>
2025-11-07 23:22:16 -0500

    test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("My name is Rashid Johnson.");
+from typing import Union
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Rashid" : "Johnson"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id":item_id, "q":q}
